ff21c99e0b13a37be8b3011b21432461ed49599f
Romain Lafourcade <romain.lafourcade@digitaslbi.fr>
2020-08-17 11:05:39 +0200

    [ADD] Use relative paths instead of absolute paths

--- index.test.js
+++ index.test.js
@@ -1,12 +1,25 @@
 import JestSimpleReporter from './'
 
 describe('onTestResult', () => {
-  const test = { path: 'foo' }
+  const test = {}
   const results = {
-    testFilePath: '/foo/bar/wombat',
+    testFilePath: 'foo/bar/wombat',
     testResults: [{
       ancestorTitles: [ 'Flargle' ],
-      failureMessages: [ 'Error: expect(jest.fn()).not.toHaveBeenCalled()\n\nExpected mock function not to be called but it was called with:\n  Array []\n    at Object.<anonymous> (/home/basie/foo/mobile/src/components/FlargleArgle/FlargleArgle.test.js:74:34)\n    at Object.asyncFn (/home/basie/foo/mobile/node_modules/jest-jasmine2/build/jasmine-async.js:68:30)\n    at resolve (/home/basie/foo/mobile/node_modules/jest-jasmine2/build/queueRunner.js:38:12)\n    at tryCallTwo (/home/basie/foo/mobile/node_modules/promise/lib/core.js:45:1)\n    at doResolve (/home/basie/foo/mobile/node_modules/promise/lib/core.js:200:9)\n    at new Promise (/home/basie/foo/mobile/node_modules/promise/lib/core.js:66:1)\n    at mapper (/home/basie/foo/mobile/node_modules/jest-jasmine2/build/queueRunner.js:31:21)\n    at Promise.resolve.then.el (/home/basie/foo/mobile/node_modules/p-map/index.js:42:16)\n    at tryCallOne (/home/basie/foo/mobile/node_modules/promise/lib/core.js:37:8)\n    at /home/basie/foo/mobile/node_modules/promise/lib/core.js:123:9' ],
+      failureMessages: [ `Error: expect(jest.fn()).not.toHaveBeenCalled()
+
+Expected mock function not to be called but it was called with:
+  Array []
+    at Object.<anonymous> (${__dirname}/src/components/FlargleArgle/FlargleArgle.test.js:74:34)
+    at Object.asyncFn (${__dirname}/node_modules/jest-jasmine2/build/jasmine-async.js:68:30)
+    at resolve (${__dirname}/node_modules/jest-jasmine2/build/queueRunner.js:38:12)
+    at tryCallTwo (${__dirname}/node_modules/promise/lib/core.js:45:1)
+    at doResolve (${__dirname}/node_modules/promise/lib/core.js:200:9)
+    at new Promise (${__dirname}/node_modules/promise/lib/core.js:66:1)
+    at mapper (${__dirname}/node_modules/jest-jasmine2/build/queueRunner.js:31:21)
+    at Promise.resolve.then.el (${__dirname}/node_modules/p-map/index.js:42:16)
+    at tryCallOne (${__dirname}/node_modules/promise/lib/core.js:37:8)
+    at ${__dirname}/node_modules/promise/lib/core.js:123:9` ],
       status: 'failed'
     }]
   }
